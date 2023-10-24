c63fd18ea27d58f913957fe388d3979dc668fc7c
Brandon Vincent <brandon@amperity.com>
2023-10-23 22:51:11 -0700

    clean up project dependencies

--- project.clj
+++ project.clj
@@ -5,22 +5,23 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
 
   :deploy-branches ["master"]
-  :pedantic? :abort
 
   :dependencies
   [[org.clojure/clojure "1.11.1"]
-   [org.clojure/tools.logging "1.2.4"]
-   [amperity/envoy "1.0.1"]
    [com.amperity/vault-clj "2.2.586"]
 
    ;; External dependencies
-   [com.amazonaws/aws-java-sdk-core "1.12.230"
-    :exclusions [org.apache.httpcomponents/httpclient
-                 org.apache.httpcomponents/httpcore]]
-   [commons-logging "1.2"]]
+   [com.amazonaws/aws-java-sdk-core "1.12.571"]]
 
   :profiles
-  {:repl
+  {;; uberjar for testing the login method on remote machines
+   :uberjar
+   {:target-path "target/uberjar"
+    :uberjar-name "vault-clj-aws.jar"
+    :main vault.auth.ext.main
+    :aot :all}
+
+   :repl
    {:source-paths ["dev"]
     :dependencies
     [[org.clojure/tools.namespace "1.3.0"]]}})
