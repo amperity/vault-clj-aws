b79ce266a52b63f434718490b75bfafd06881a32
Brandon Vincent <brandon@amperity.com>
2025-12-24 00:45:26 -0800

    SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.amperity/vault-clj-aws "2.0.0"
+(defproject com.amperity/vault-clj-aws "2.0.0-SNAPSHOT"
   :description "Vault-clj extension that implements the AWS auth method."
   :url "https://github.com/amperity/vault-clj-aws"
   :license {:name "Apache License"
