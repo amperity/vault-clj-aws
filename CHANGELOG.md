b80f3b716efcc20fdd0059dc63e558ca0493f0d1
Brandon Vincent <brandonvin@gmail.com>
2026-02-25 12:53:54 -0800

    Merge pull request #7 from amperity/vault-clj-2
    vault-clj 2.x rewrite

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,7 +6,16 @@ This change log follows the conventions of [keepachangelog.com](http://keepachan
 
 ## [Unreleased]
 
-...
+- The library was essentially rewritten. Check README.md for updated usage info.
+  Most importantly, this library is now based on `com.amperity/vault-clj` 2.x.
+  Additionally:
+    - For consistency with other Amperity open-source, the artifact has been renamed to `com.amperity/vault-clj-aws`
+      and the default branch changed to `main`.
+    - Reasonable defaults: the login method now uses the AWS Java SDK's default
+      credentials and default region if none are provided.
+    - The legacy AWS Java SDK v1 has been replaced with AWS Java SDK v2.
+    - The dependency on `cheshire` / `jackson` for JSON has been removed in favor of `org.clojure/data.json`.
+    - The vestigial dependency on `amperity/envoy` has been removed.
 
 ## [0.0.4] - 2022-06-02
 
--- dev/user.clj
+++ dev/user.clj
@@ -1,8 +1,39 @@
 (ns user
   (:require
-    [cheshire.core :as json]
-    [clj-http.client :as http]
-    [clojure.java.io :as io]
-    [clojure.tools.namespace.repl :refer [refresh]]
-    [vault.client.ext.aws :as ext]
-    [vault.core :as vault]))
+    [vault.auth.aws :as aws]
+    [vault.client :as vault])
+  (:import
+    (software.amazon.awssdk.auth.credentials
+      AwsSessionCredentials)))
+
+
+(def vault-addr
+  "https://vault.example.com:8200")
+
+
+(def vault-client
+  nil)
+
+
+(defn start
+  []
+  (alter-var-root #'vault-client (constantly (vault/new-client vault-addr)))
+  (alter-var-root #'vault-client vault/start))
+
+
+(defn stop
+  []
+  (alter-var-root #'vault-client vault/stop)
+  (alter-var-root #'vault-client (constantly nil)))
+
+
+(comment
+  (def session-credentials
+    (AwsSessionCredentials/create
+      "access-key-id"
+      "secret-access-key"
+      "session-token"))
+  (aws/login
+    vault-client
+    {:aws-region "us-east-1"
+     :aws-credentials session-credentials}))
